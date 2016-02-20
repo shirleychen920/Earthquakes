a52147cc6a0daa235d7e71c652b519cd37fac678
shirleychen920 <chenhaojie94@gmail.com>
2016-02-20 01:32:13 -0800

    add timer

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-  var REQUEST_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
+  
+  
+  
+  var REQUEST_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson';
   var earthquakeContainer = $('#earthquakeContainer');
   var responses = {
   earthquake:[],  
   firstLoad: false,
   hasError: false
   };
+
   var template = Handlebars.compile($('#earthquakeTemplate').html());
-  $('#getAnotherEarthquake').click(getEarthquake);
-  getEarthquake();
+   $('#getAnotherEarthquake').click(getEarthquake());
+    getEarthquake();
+ 
 
 
   function getEarthquake() {
